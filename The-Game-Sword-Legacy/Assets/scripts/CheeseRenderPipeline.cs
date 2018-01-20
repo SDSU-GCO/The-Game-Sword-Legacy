e716df03474188c4a53607d6a3c05bea0e6bac77
Alex Evans <alexander.evans@jacks.sdstate.edu>
2018-01-19 23:40:46 -0600

    Created custom lighting engine rendering scriptible object asset
    unity 2017.3.0f3

---
+++ The-Game-Sword-Legacy/Assets/scripts/CheeseRenderPipeline.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace CheeseRenderNamespace
+{
+    public class CheeseRenderPipeline : RenderPipeline
+    {
+        private CheesePipelineAsset assetReference;
+        public CheeseRenderPipeline(CheesePipelineAsset cheesePipelineAsset)
+        {
+            assetReference = cheesePipelineAsset; //shouldn't be null
+        }
+
+        public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+        {
+            base.Render(renderContext, cameras);
+        }
+
+    }
+}
