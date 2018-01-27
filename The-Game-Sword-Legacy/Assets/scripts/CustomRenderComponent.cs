607d07cb9bb197dc9b02bfe2fffe6f818472af31
Genzo Namikawa <30657191+gnamikawa@users.noreply.github.com>
2018-01-27 06:46:58 -0600

    Goal 1 90% Finished
    Finished rendering a simple background with CommandBuffer pooling.

---
+++ The-Game-Sword-Legacy/Assets/scripts/CustomRenderComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomRenderComponent : MonoBehaviour {
+
+    private GameObject i;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
